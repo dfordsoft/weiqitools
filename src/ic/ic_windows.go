0fb56e751f52e1ca51c6449e5202bb87d831b090
Fan Yang <missdeer@dfordsoft.com>
2016-10-07 21:18:29 +0800

    (*)pure go implementation convert between CJK encodings and UTF-8

--- src/ic/ic_windows.go
+++ src/ic/ic_windows.go
@@ -4,24 +4,24 @@ package ic
 import "fmt"
 
 func Convert(from string, to string, src []byte) []byte {
-	if (from != "gbk" && from != "utf-8") || (to != "gbk" && to != "utf-8") {
-		fmt.Println("only converting between GBK and UTF-8 is supported")
-		return src
-	}
-	if from == "gbk" && to == "utf-8" {
-		if out, e := GbkToUtf8(src); e == nil {
+	if to == "utf-8" {
+		if out, e := ToUTF8(from, src); e == nil {
 			return out
 		} else {
-			fmt.Println("converting from gbk to utf-8 failed", e)
+			fmt.Printf("converting from %s to UTF-8 failed: %v", from, e)
+			return src
 		}
 	}
-	if to == "gbk" && from == "utf-8" {
-		if out, e := Utf8ToGbk(src); e == nil {
+
+	if from == "utf-8" {
+		if out, e := FromUTF8(to, src); e != nil {
 			return out
 		} else {
-			fmt.Println("converting from utf-8 to gbk failed", e)
+			fmt.Printf("converting from UTF-8 to %s failed: %v", to, e)
+			return src
 		}
 	}
+	fmt.Println("only converting between CJK encodings and UTF-8 is supported")
 	return src
 }
 
