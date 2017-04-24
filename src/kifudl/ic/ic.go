f154fef7a9c03ce380d19ccc0f8e79056f580f4b
Fan Yang <me@minidump.info>
2017-04-24 10:49:01 +0800

    (*)remove iconv dependency

--- src/kifudl/ic/ic.go
+++ src/kifudl/ic/ic.go
@@ -1,32 +1,29 @@
-// +build !windows
-
 package ic
 
-import (
-	"log"
-
-	"gopkg.in/iconv.v1"
-)
+import "log"
 
 func Convert(from string, to string, src []byte) []byte {
-	cd, err := iconv.Open(to, from)
-	if err != nil {
-		log.Println("iconv.Open failed!")
+	if to == "utf-8" {
+		out, e := ToUTF8(from, src)
+		if e == nil {
+			return out
+		}
+		log.Printf("converting from %s to UTF-8 failed: %v", from, e)
 		return src
 	}
-	defer cd.Close()
 
-	outbuf := make([]byte, len(src)*4)
-	out, _, _ := cd.Conv(src, outbuf)
-	return out
+	if from == "utf-8" {
+		out, e := FromUTF8(to, src)
+		if e == nil {
+			return out
+		}
+		log.Printf("converting from UTF-8 to %s failed: %v", to, e)
+		return src
+	}
+	log.Println("only converting between CJK encodings and UTF-8 is supported")
+	return src
 }
 
 func ConvertString(from string, to string, src string) string {
-	cd, err := iconv.Open(to, from)
-	if err != nil {
-		log.Println("iconv.Open failed!")
-		return src
-	}
-	defer cd.Close()
-	return cd.ConvString(src)
+	return string(Convert(from, to, []byte(src)))
 }
